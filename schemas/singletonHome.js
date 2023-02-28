e2255ccac5a09be9e1b6597034c96115e1b4c0fe
Sam Goddard <hello@samgoddard.co.uk>
2023-02-28 10:52:12 +0000

    new home fields

--- schemas/singletonHome.js
+++ schemas/singletonHome.js
@@ -27,6 +27,38 @@ export default {
       validation: Rule => Rule.required()
     },
     {
+      title: 'Background Text',
+      name: 'backgroundText',
+      type: 'array', 
+      of: [{type: 'block'}],
+      validation: Rule => Rule.required()
+    },
+    {
+      title: 'People Text',
+      name: 'peopleText',
+      type: 'text',
+      rows: 5,
+      validation: Rule => Rule.required()
+    },
+    {
+      title: 'Expertise',
+      name: 'expertise',
+      type: 'array',
+      of: [{
+        type: 'string',
+        validation: Rule => Rule.required()
+      }],
+    },
+    {
+      title: 'Partners',
+      name: 'partners',
+      type: 'array',
+      of: [{
+        type: 'string',
+        validation: Rule => Rule.required()
+      }],
+    },
+    {
       title: 'SEO / Share Settings',
       name: 'seo',
       type: 'seo'
